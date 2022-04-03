2eab3a495d05a676d5e8cadde46c0c27e66cc775
XCkiro <xckiro@gmail.com>
2022-04-04 05:20:19 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -9,4 +9,4 @@ cd mtg-2.1.6-linux-amd64
 chmod u+x mtg
 # run
 echo $PORT
-./mtg simple-run -n 1.1.1.1 -t 30s -a 512kib 0.0.0.0:443 7jZ6GJruGPoxwZAFTv1KjpVzdG9yYWdlLmdvb2dsZWFwaXMuY29t
+# ./mtg simple-run -n 1.1.1.1 -t 30s -a 512kib 0.0.0.0:443 7jZ6GJruGPoxwZAFTv1KjpVzdG9yYWdlLmdvb2dsZWFwaXMuY29t
