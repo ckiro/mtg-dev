311d19531f8e9cdef68221246bfeaedce0796add
XCkiro <xckiro@gmail.com>
2022-04-04 04:27:32 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -8,4 +8,5 @@ cd mtg-2.1.6-darwin-amd64
 # run permission
 chmod u+x mtg
 # run
+echo $PORT
 ./mtg simple-run -n 1.1.1.1 -t 30s -a 512kib 127.0.0.1:$PORT 7jZ6GJruGPoxwZAFTv1KjpVzdG9yYWdlLmdvb2dsZWFwaXMuY29t
