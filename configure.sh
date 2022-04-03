8610ecd7560122417b3030deb429e25eff7bbcfd
XCkiro <xckiro@gmail.com>
2022-04-04 04:36:40 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # download MTG
-wget https://github.com/9seconds/mtg/releases/download/v2.1.6/mtg-2.1.6-linux-amd64.tar.gz
+wget https://github.com/9seconds/mtg/releases/download/v2.1.6/mtg-2.1.6-darwin-amd64.tar.gz
 # decompress
-tar -xzvf mtg-2.1.6-linux-amd64.tar.gz
+tar -xzvf mtg-2.1.6-darwin-amd64.tar.gz
 # cd
-cd mtg-2.1.6-linux-amd64
+cd mtg-2.1.6-darwin-amd64
 # run permission
 chmod u+x mtg
 # run
 echo $PORT
-./mtg simple-run -n 1.1.1.1 -t 30s -a 512kib 127.0.0.1:$PORT 7jZ6GJruGPoxwZAFTv1KjpVzdG9yYWdlLmdvb2dsZWFwaXMuY29t
+./mtg simple-run -n 1.1.1.1 -t 30s -a 512kib 0.0.0.0:$PORT 7jZ6GJruGPoxwZAFTv1KjpVzdG9yYWdlLmdvb2dsZWFwaXMuY29t
